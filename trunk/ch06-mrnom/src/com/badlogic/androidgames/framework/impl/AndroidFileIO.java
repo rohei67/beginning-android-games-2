98dc145139208c8746b706430a1e7d2cbc70914e
rgreen@batterypoweredgames.com <rgreen@batterypoweredgames.com@d47f994e-9548-8889-52ce-d0b02799f726>
2011-08-26 20:12:04 +0000

    Ch 4 and 6 updates

--- trunk/ch06-mrnom/src/com/badlogic/androidgames/framework/impl/AndroidFileIO.java
+++ trunk/ch06-mrnom/src/com/badlogic/androidgames/framework/impl/AndroidFileIO.java
@@ -7,17 +7,22 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.AssetManager;
 import android.os.Environment;
+import android.preference.PreferenceManager;
 
 import com.badlogic.androidgames.framework.FileIO;
 
 public class AndroidFileIO implements FileIO {
+	Context context;
     AssetManager assets;
     String externalStoragePath;
 
-    public AndroidFileIO(AssetManager assets) {
-        this.assets = assets;
+    public AndroidFileIO(Context context) {
+        this.context = context;
+        this.assets = context.getAssets();
         this.externalStoragePath = Environment.getExternalStorageDirectory()
                 .getAbsolutePath() + File.separator;
     }
@@ -35,5 +40,9 @@ public class AndroidFileIO implements FileIO {
     @Override
     public OutputStream writeFile(String fileName) throws IOException {
         return new FileOutputStream(externalStoragePath + fileName);
+    }
+    
+    public SharedPreferences getPreferences() {
+    	return PreferenceManager.getDefaultSharedPreferences(context);
     }
 }
